97da36122a23c234b1761712ef527cdba51659fe
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-04-29 01:36:03 +0200

    Add event for states

--- src/modules/ClassStore.js
+++ src/modules/ClassStore.js
@@ -1,3 +1,4 @@
+/* eslint-disable eqeqeq */
 export default class Tasks {
   constructor() {
     this.tasksArray = JSON.parse(localStorage.getItem('tasks')) || [];
@@ -6,14 +7,12 @@ export default class Tasks {
   displayTasks() {
     const taskDiv = document.querySelector('.todo-tasks');
     taskDiv.innerHTML = '';
-    this.tasksArray.sort((taskOne, taskTwo) => { //eslint-disable-line
-    return taskOne.index - taskTwo.index; //eslint-disable-line
-    }).forEach((task) => {
+    this.tasksArray.sort((taskOne, taskTwo) => taskOne.index - taskTwo.index).forEach((task) => {
       const taskElement = document.createElement('div');
       taskElement.classList.add('task');
       taskDiv.appendChild(taskElement);
       taskElement.innerHTML = `
-        <input type="checkbox" id="${task.index}" />
+        <input class="checkbox" type="checkbox" id="${task.index}" />
         <textarea class="textarea" for="${task.index}">${task.description}</textarea>
         <span class="delete">&cross;</span>
        `;
@@ -28,17 +27,59 @@ export default class Tasks {
     const textareaEvent = document.querySelectorAll('.textarea');
     for (let i = 0; i < textareaEvent.length; i += 1) {
       textareaEvent[i].addEventListener('focusout', (e) => {
+        e.target.parentElement.style.background = '#fff';
         if (e.target.value) {
           this.updateTask(e.target.parentElement.firstElementChild.id, e.target.value);
-        } else return; //eslint-disable-line
+        }
+        if (e.target.previousElementSibling.checked) {
+          e.target.style.textDecoration = 'line-through';
+        }
+      });
+      textareaEvent[i].addEventListener('focusin', (e) => {
+        e.target.parentElement.style.background = '#E9E4F0';
+        e.target.style.background = 'transparent';
+        e.target.style.textDecoration = 'none';
       });
     }
+
+    const checkboxInputs = document.querySelectorAll('input[type=checkbox]');
+    for (let i = 0; i < checkboxInputs.length; i += 1) {
+      checkboxInputs[i].addEventListener('change', (e) => {
+        for (let task of this.tasksArray) {//eslint-disable-line
+          if (task.index == e.target.id) {
+            if (e.target.checked) {
+              task.complete = true;
+              e.target.nextElementSibling.style.textDecoration = 'line-through';
+            } else {
+              task.complete = false;
+              e.target.nextElementSibling.style.textDecoration = 'none';
+            }
+          }
+        }
+        localStorage.setItem('tasks', JSON.stringify(this.tasksArray));
+      });
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+      const checkedBoxs = document.querySelectorAll('.checkbox');
+      for (let i = 0; i < checkedBoxs.length; i += 1) {
+        for (let task of this.tasksArray) {//eslint-disable-line
+          if (checkedBoxs[i].id == task.index) {
+            if (task.complete == true) {
+              checkedBoxs[i].checked = true;
+            } else {
+              checkedBoxs[i].checked = false;
+            }
+          }
+        }
+      }
+    });
   }
 
   addTask(description) {
     const task = {
       description,
-      index: this.tasksArray.length + 1,
+      index: Date.now().toString(),
       complete: false,
     };
     this.tasksArray.push(task);
@@ -53,8 +94,8 @@ export default class Tasks {
   }
 
   updateTask(index, newdescription) {
-    for (let task of this.tasksArray) { //eslint-disable-line
-      if (task.index == index) {  //eslint-disable-line
+    for (let task of this.tasksArray) {//eslint-disable-line
+      if (task.index == index) {
         task.description = newdescription;
       }
     }
